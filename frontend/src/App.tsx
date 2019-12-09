db28d4b031e660e7f8873bad17bfea6d4020bbb6
Alexandre <aflordlc@gmail.com>
2019-12-09 16:21:31 +0100

    New Structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Home from "./Home";
+import Login from "./Login";
 
 const App: React.FC = () => {
   return (
