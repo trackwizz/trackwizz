69380cea21a08d15de1473b8409e327954898e88
nockty <corentin.dupret@student.ecp.fr>
2019-12-16 18:45:46 +0100

    :recycle: Use environment variable for server port

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,17 +11,13 @@ mod tests;
 
 use database::connect_to_database;
 use postgres::Connection;
-use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let port = &args[1];
-
     let conn:Connection = match connect_to_database() {
         Ok(conn) => conn,
         Err(error) => panic!("Could not connect to database: \n\t{}", error)
     };
     println!("Successfully Connected to db: {}", conn.is_active());
 
-    server::start(port);
+    server::start();
 }
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,12 +1,14 @@
 use actix_web::{App, HttpServer};
 use crate::controllers::{test_controller};
+use crate::utils::get_env_variable;
 
-pub fn start(port: &str) {
+pub fn start() {
+    let port: String = get_env_variable("PORT", "8080");
     HttpServer::new(|| {
         App::new()
             .configure(test_controller::routes)
     })
-        .bind(format!("0.0.0.0:{}", port))
+        .bind(format!("0.0.0.0:{}", &port))
         .unwrap()
         .run()
         .unwrap();
