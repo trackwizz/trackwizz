ce52ab6f96c7d7f48cc684416961e56fb95a78df
David Robert-Ansart <david.robertansart@gmail.com>
2019-12-16 19:45:25 +0100

    :fire: Remove test object related files

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,10 +1,9 @@
 use actix_web::{App, HttpServer};
-use crate::controllers::{test_controller, user_controller};
+use crate::controllers::{user_controller};
 
 pub fn start(port: &str) {
     HttpServer::new(|| {
         App::new()
-            .configure(test_controller::routes)
             .configure(user_controller::routes)
     })
         .bind(format!("0.0.0.0:{}", port))
