e0f67edb71368cec5a9bdc47bce5c54cb29887fd
Victor Paltz <paltz.victor@free.fr>
2019-12-16 16:58:10 +0100

    update

--- server/src/controllers/login_controller.rs
+++ server/src/controllers/login_controller.rs
@@ -1,7 +1,13 @@
+use crate::models::test_model::TestObj;
+
 use actix_web::{guard, http, web, HttpRequest, HttpResponse};
 
-use crate::models::test_model::TestObj;
-use serde_json::json;
+use reqwest;
+use reqwest::Client;
+
+//use serde_json;
+
+//use serde_json::json;
 
 //const redirect_uri = "http://localhost:8888/callback";
 //const front_redirect_uri = "http://localhost:3000/login?";
@@ -39,20 +45,22 @@ pub struct LoginConfig {
     pub state: String,
 }
 
-
-
 //#TODO do not hardcode client_id,
 //#TODO generate state randomly.
 
 fn login(_req: HttpRequest) -> HttpResponse {
     println!("goooooood on est là!");
-    HttpResponse::Ok().json(json!({
-        "response_type": String::from("code"),
-        "client_id": String::from("b40d05324ed744d0b7c593f04d6e6821"),
-        "scope": String::from("user-read-private user-read-email user-read-playback-state"),
-        "redirect_uri": String::from("http://localhost:5000/callback"),
-        "state": String::from("118218")
-    }))
+
+    let login_config = LoginConfig {
+        response_type: String::from("code"),
+        client_id: String::from("b40d05324ed744d0b7c593f04d6e6821"),
+        scope: String::from("user-read-private user-read-email user-read-playback-state"),
+        redirect_uri: String::from("http://localhost:5000/callback"),
+        state: String::from("118218"),
+    };
+
+    HttpResponse::Ok().json(login_config)
+    // HttpResponse::Ok().json()
 }
 
 #[derive(Serialize, Deserialize)]
@@ -75,11 +83,18 @@ pub struct Form {
 #[derive(Serialize, Deserialize)]
 pub struct Header {
     #[serde(default)]
-    pub Authorization: String,
+    pub authorization: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TokensResponse {
+    #[serde(default)]
+    pub access_token: String,
+    pub refresh_token: String,
+    pub expires_in: String,
 }
 
 fn callback(_req: HttpRequest) -> HttpResponse {
-
     //let code = String::from(_req.query().get("code"));
     //let state = String::from(_req.query().get("state"));
 
@@ -98,15 +113,76 @@ fn callback(_req: HttpRequest) -> HttpResponse {
 
     println!("coucou on est là!");
 
-    HttpResponse::Ok().json(LoginConfig {
-        response_type: String::from("code"),
-        client_id: String::from("b40d05324ed744d0b7c593f04d6e6821"),
-        scope: String::from("user-read-private user-read-email user-read-playback-state"),
-        redirect_uri: String::from("http://localhost:5000/callback"),
-        state: String::from("118218"),
-    })
-}
+    let authOptions = AuthOptions {
+        url: String::from("https://accounts.spotify.com/api/token"),
+        form: Form {
+            code: String::from("code"),
+            redirect_uri: String::from("http://localhost:5000/callback"),
+            grant_type: String::from("authorization_code"),
+        },
+        headers: Header {
+            authorization: String::from(
+                "Basic b40d05324ed744d0b7c593f04d6e6821:cfe0feb180e84c7bbc5794e276618924",
+            ),
+        },
+        json: String::from("http://localhost:5000/callback"),
+    };
 
+    let mut response:Result<TokensResponse, Error> = match Client::new()
+        .post(&authOptions.url)
+        .basic_auth(
+            String::from("b40d05324ed744d0b7c593f04d6e6821"),
+            Some(String::from("cfe0feb180e84c7bbc5794e276618924")),
+        )
+        .json(&authOptions)
+        .send()?
+        .json()?;
+
+    match response
+    {
+        Ok(val) => val,
+        Err(error) => return HttpResponse::Ok().json(authOptions),
+    };
+
+    //response.json()?
+
+    // match response.json() {
+    //     Ok(val) => return val,
+    //     Err(error) => return HttpResponse::Ok().json(authOptions),
+    // };
+
+    HttpResponse::Ok().json(response)
+
+    // let gist: Gist = response.json()?;
+
+    // fn main() -> Result<()> {
+    //     let gh_user = env::var("GH_USER")?;
+    //     let gh_pass = env::var("GH_PASS")?;
+    //     let gist_body = json!({
+    //         "description": "the description for this gist",
+    //         "public": true,
+    //         "files": {
+    //              "main.rs": {
+    //              "content": r#"fn main() { println!("hello world!");}"#
+    //             }
+    //         }});
+    //     let request_url = "https://api.github.com/gists";
+    //     let mut response = Client::new()
+    //         .post(request_url)
+    //         .basic_auth(gh_user.clone(), Some(gh_pass.clone()))
+    //         .json(&gist_body)
+    //         .send()?;
+    //     let gist: Gist = response.json()?;
+    //     println!("Created {:?}", gist);
+    //     let request_url = format!("{}/{}",request_url, gist.id);
+    //     let response = Client::new()
+    //         .delete(&request_url)
+    //         .basic_auth(gh_user, Some(gh_pass))
+    //         .send()?;
+    //     println!("Gist {} deleted! Status code: {}",gist.id, response.status());
+    //     Ok(())
+    // }
+}
 
 fn get_one(req: HttpRequest) -> HttpResponse {
     match req.match_info().get("id").unwrap().parse::<u32>() {
