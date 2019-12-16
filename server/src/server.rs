2b3aa9e0957cd7eb2716a1e4f505403a1db82270
David Robert-Ansart <david.robertansart@gmail.com>
2019-12-16 20:04:16 +0100

    :loud_sound: Add server requests log

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,9 +1,16 @@
 use actix_web::{App, HttpServer};
+use actix_web::middleware::Logger;
+use env_logger;
+
 use crate::controllers::{user_controller};
 
 pub fn start(port: &str) {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::new("%a \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %Dms"))
             .configure(user_controller::routes)
     })
         .bind(format!("0.0.0.0:{}", port))
