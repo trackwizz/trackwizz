3f13e4cd9530d08c28edcf8fd96666cc8b41a4b0
Alexandre <aflordlc@gmail.com>
2019-12-18 14:31:16 +0100

    WIP: Continue game

--- frontend/src/pages/Game/Quizz/index.tsx
+++ frontend/src/pages/Game/Quizz/index.tsx
@@ -1,78 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import dancers from "../../../utils/dancers";
-import axios from "axios";
 import "./quizz.css";
-import { CookieKey, isTokenValid } from "../../../utils/auth";
-import Cookies from "universal-cookie";
 
-const cookies = new Cookies();
+interface IQuizz {
+  track: string;
+  choices: string[];
+  incrementScore: () => void;
+}
 
-const Quizz: React.FC<{ choices: string[] }> = ({ choices }) => {
+const Quizz: React.FC<IQuizz> = ({
+  track,
+  choices,
+  incrementScore
+}: IQuizz) => {
   const [dancer] = useState<string>(
     dancers[Math.floor(Math.random() * dancers.length)]
   );
-  const [track, setTrack] = useState<string | null>(null);
-  const [isPlaying, setIsPlaying] = useState<boolean>(true);
 
-  useEffect(() => {
-    (async (): Promise<void> => {
-      if (isTokenValid()) {
-        const res = await axios.get(
-          "https://api.spotify.com/v1/search?q=muse&type=track&market=FR",
-          {
-            headers: {
-              Authorization: `Bearer ${cookies.get(CookieKey.ACCESS_TOKEN)}`
-            }
-          }
-        );
-        console.log(res.data.tracks.items[2]);
-        setTrack(res.data.tracks.items[2].preview_url);
-      }
-    })();
-  }, []);
+  const handleAnswer = () => () => {
+    // TODO : Faire requete au back
+    const isAnswerTrue = true;
 
-  const onPlayClicked = (): void => {
-    (document.getElementById("player") as HTMLAudioElement).play();
-  };
-  const onPausedClicked = (): void => {
-    (document.getElementById("player") as HTMLAudioElement).pause();
-  };
-
-  const onButtonClick = (): void => {
-    if (isPlaying) {
-      onPausedClicked();
-    } else {
-      onPlayClicked();
+    if (isAnswerTrue) {
+      incrementScore();
     }
-    setIsPlaying(!isPlaying);
+
+    return;
   };
 
   return (
     <div className="flex-container column">
       <div className="text-center">
         <h2 className="fancy-text">Which song is currently playing?</h2>
-        <img
-          height="180px"
-          src={dancer}
-          alt="dancer"
-          className={isPlaying ? "" : "hidden"}
-        />
+        <img height="180px" src={dancer} alt="dancer" />
       </div>
-      <button
-        className="play-button"
-        onClick={onButtonClick}
-        style={{
-          margin: "0 0 0.5rem",
-          padding: isPlaying ? "0" : "0 0 0 0.4rem"
-        }}
-      >
-        {isPlaying ? "❙❙" : "►"}
-      </button>
       <div className="grid-container">
         <div>
           <div className="grid">
             {choices.map((c, index) => (
-              <button key={index}>{c}</button>
+              <button key={index} onClick={handleAnswer()}>
+                {c}
+              </button>
             ))}
           </div>
         </div>
--- frontend/src/pages/Game/index.tsx
+++ frontend/src/pages/Game/index.tsx
@@ -3,9 +3,12 @@ import { Redirect } from "react-router";
 import Quizz from "./Quizz";
 import "./game.css";
 import { isTokenValid } from "../../utils/auth";
+import Score from "./Score";
 
 const Game: React.FC = () => {
-  const [countdown, setCountdown] = useState<number>(3);
+  const [countdown, setCountdown] = useState<number>(0);
+  const [score, setScore] = useState<number>(0);
+  const [hasEnded, setHasEnded] = useState<boolean>(false);
 
   useEffect(() => {
     setTimeout(() => {
@@ -15,19 +18,34 @@ const Game: React.FC = () => {
     }, 1000);
   }, [countdown]);
 
+  const incrementScore = () => {
+    setScore(score + 1);
+  };
+
   if (!isTokenValid()) {
     return <Redirect to="/login" />;
   }
 
   return (
     <div className="height-100">
-      {countdown > 0 ? (
+      {countdown > 0 && (
         <div className="flex-container">
           <div className="countdown scale-in-center">{countdown}</div>
         </div>
-      ) : (
-        <Quizz choices={["Highway to hell", "Supermassive Black Hole", "Dancing Queen", "The answer D"]} />
       )}
+      {countdown === 0 && hasEnded === false && (
+        <Quizz
+          choices={[
+            "Highway to hell",
+            "Supermassive Black Hole",
+            "Dancing Queen",
+            "The answer D"
+          ]}
+          setHasEnded={setHasEnded}
+          incrementScore={incrementScore}
+        />
+      )}
+      {countdown === 0 && hasEnded === true && <Score score={score} />}
     </div>
   );
 };
