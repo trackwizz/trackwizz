97463f5b6f7a2c8edcc5b1438fe34ebc4ab88b0c
Corentin Carteau <corentin.carteau.intern@birdie.care>
2020-01-06 15:28:40 +0100

    remove usage of test controller

--- server/src/controllers/login_controller.rs
+++ server/src/controllers/login_controller.rs
@@ -1,5 +1,3 @@
-use crate::models::test_model::TestObj;
-
 use actix_web::{guard, http, web, HttpRequest, HttpResponse};
 
 use reqwest;
@@ -17,20 +15,6 @@ pub fn routes(cfg: &mut web::ServiceConfig) {
         web::scope("/")
             .route("/login", web::get().to(login))
             .route("/callback", web::get().to(callback))
-            .route(
-                "",
-                web::post()
-                    .guard(guard::Header("content-type", "application/json"))
-                    .to(create),
-            )
-            .route(
-                "/{id}",
-                web::put()
-                    .guard(guard::Header("content-type", "application/json"))
-                    .to(edit),
-            )
-            .route("/{id}", web::get().to(get_one))
-            .route("/{id}", web::delete().to(delete)),
     );
 }
 use serde::{Deserialize, Serialize};
@@ -182,38 +166,4 @@ fn callback(_req: HttpRequest) -> HttpResponse {
     //     println!("Gist {} deleted! Status code: {}",gist.id, response.status());
     //     Ok(())
     // }
-}
-
-fn get_one(req: HttpRequest) -> HttpResponse {
-    match req.match_info().get("id").unwrap().parse::<u32>() {
-        Ok(id) => HttpResponse::Ok().json(TestObj {
-            id,
-            name: "Get one".parse().unwrap(),
-        }),
-        Err(_) => HttpResponse::new(http::StatusCode::NOT_FOUND),
-    }
-}
-
-fn create(_req: HttpRequest, new_test_obj: web::Json<TestObj>) -> HttpResponse {
-    HttpResponse::Ok().json(TestObj {
-        id: 1,
-        name: new_test_obj.name.to_string(),
-    })
-}
-
-fn edit(req: HttpRequest, updated_test_obj: web::Json<TestObj>) -> HttpResponse {
-    match req.match_info().get("id").unwrap().parse::<u32>() {
-        Ok(id) => HttpResponse::Ok().json(TestObj {
-            id,
-            name: updated_test_obj.name.to_string(),
-        }),
-        Err(_) => HttpResponse::new(http::StatusCode::NOT_FOUND),
-    }
-}
-
-fn delete(req: HttpRequest) -> HttpResponse {
-    match req.match_info().get("id").unwrap().parse::<u32>() {
-        Ok(_) => HttpResponse::new(http::StatusCode::NO_CONTENT),
-        Err(_) => HttpResponse::new(http::StatusCode::NOT_FOUND),
-    }
 }
