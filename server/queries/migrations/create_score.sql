6ed5419c55d9bcea214114d8823337ce3bc092e5
quentinverlhac <quentin.verlhac@gmail.com>
2020-01-13 16:30:52 +0100

    :card_file_box: change timestamp type to int

--- server/queries/migrations/create_score.sql
+++ server/queries/migrations/create_score.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS Score (
     id_game             INT       NOT NULL,
     id_user             INT       NOT NULL,
     spotify_track_id    VARCHAR(50) UNIQUE,
-    timestamp           TIMESTAMP NOT NULL,
+    timestamp           BIGINT    NOT NULL,
     is_correct          BOOLEAN   NOT NULL,
     reaction_time       INT       NOT NULL,
     CONSTRAINT Score_Game_FK FOREIGN KEY (id_game) REFERENCES Game (id)
