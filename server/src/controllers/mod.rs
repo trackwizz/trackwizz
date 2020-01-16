75c5a8dc53402ceb09ef88743db1e23a6d3a77b7
Victor Paltz <paltz.victor@free.fr>
2020-01-16 19:25:05 +0100

    small lint

--- server/src/controllers/mod.rs
+++ server/src/controllers/mod.rs
@@ -2,6 +2,6 @@ pub mod game_controller;
 pub mod genre_controller;
 pub mod playlist_controller;
 mod playlist_tracks_controller;
+pub mod score_controller;
 pub mod track_controller;
 pub mod user_controller;
-pub mod score_controller;
