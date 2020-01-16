66b27a5e0aa72e7f29bb30d1b27e4fec16503c9d
Alexandre <aflordlc@gmail.com>
2020-01-16 10:22:33 +0100

    Add axiosRequest file

---
+++ frontend/src/pages/ConnectedPages/components/Dancers/axiosRequest.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+interface IResponse<T> {
+  data: null | T;
+  error: null | boolean;
+  complete: null | boolean;
+}
+
+async function axiosRequest<T>(req: AxiosRequestConfig): Promise<IResponse<T>> {
+  let response: IResponse<T> = {
+    data: null,
+    error: null,
+    complete: null
+  };
+
+  await axios(req)
+    .then(
+      res =>
+        (response = {
+          data: res.data,
+          error: false,
+          complete: true
+        })
+    )
+    .catch(
+      () =>
+        (response = {
+          data: null,
+          error: true,
+          complete: true
+        })
+    );
+
+  return response;
+}
+
+export { axiosRequest };
