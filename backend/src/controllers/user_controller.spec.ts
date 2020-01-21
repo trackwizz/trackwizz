c3bffda2d40ea3c72f7f5f621eef61141bdd5543
nockty <corentin.dupret@student.ecp.fr>
2020-01-21 14:47:56 +0100

    :white_check_mark: Add tests for the user controller

---
+++ backend/src/controllers/user_controller.spec.ts
@@ -0,0 +1,93 @@
+import request from 'supertest';
+import server from '../server';
+import { User } from '../entities/user';
+import { getConnection, getRepository } from 'typeorm';
+
+let user: User;
+
+async function createUser() {
+    const user: User = new User();
+    user.name = "John";
+    await getRepository(User).save(user);
+    return user;
+}
+
+beforeAll(async () => {
+    user = await createUser();
+})
+
+afterAll(async () => {
+    await getConnection()
+        .createQueryBuilder()
+        .delete()
+        .from(User)
+        .execute();
+})
+
+describe('Test GET ALL', () => {
+    it('should return pre-created user as a list', async () => {
+        const res = await request(server)
+            .get('/users')
+            .send();
+        expect(res.status).toEqual(200);
+        expect(res.body.length).toBe(1);
+        expect(res.body[0].id).toBe(user.id);
+        expect(res.body[0].name).toBe(user.name);
+    })
+})
+
+describe('Test GET, DELETE, GET user', () => {
+    it('should return pre-created user', async () => {
+        const res = await request(server)
+            .get(`/users/${user.id}`)
+            .send();
+        expect(res.status).toEqual(200);
+        expect(res.body.id).toBe(user.id);
+        expect(res.body.name).toBe(user.name);
+    }),
+        it('should delete pre-created user', async () => {
+            const res = await request(server)
+                .delete(`/users/${user.id}`)
+                .send();
+            expect(res.status).toEqual(204);
+        }),
+        it('should not return pre-created user', async () => {
+            const res = await request(server)
+                .get(`/users/${user.id}`)
+                .send();
+            expect(res.status).toEqual(404);
+        })
+})
+
+describe('Test POST, PUT, GET', () => {
+    const user: User = new User();
+    user.name = "Mickey";
+    it('should create a new user', async () => {
+        const res = await request(server)
+            .post('/users')
+            .send({
+                name: user.name,
+            });
+        expect(res.status).toEqual(200);
+        console.log(res.body);
+        user.id = res.body.id;
+    }),
+        it('should update the user', async () => {
+            const res = await request(server)
+                .put(`/users/${user.id}`)
+                .send({
+                    name: "Donald",
+                });
+            expect(res.status).toEqual(200);
+            console.log(res.body);
+        }),
+        it('should get the updated user', async () => {
+            const res = await request(server)
+                .get(`/users/${user.id}`)
+                .send();
+            expect(res.status).toEqual(200);
+            expect(res.body.id).toBe(user.id);
+            expect(res.body.name).toBe("Donald");
+        })
+})
+
