399997261dc0d45650cb43d33e4e49530df4ebb8
nockty <corentin.dupret@student.ecp.fr>
2020-01-21 14:46:58 +0100

    :white_check_mark: Add simple test for server and DB

---
+++ backend/src/server.spec.ts
@@ -0,0 +1,19 @@
+import request from 'supertest';
+import server from './server';
+import { getConnection } from 'typeorm';
+
+describe('Test server is running', () => {
+    it('should return hello world', async () => {
+        const res = await request(server)
+            .get('/')
+            .send();
+        expect(res.status).toEqual(200);
+        expect(res.text).toBe('TrackWizz server is running!');
+    })
+})
+
+describe('Test DB connection', () => {
+    it('should connect to DB', async () => {
+        expect(await getConnection().isConnected).toBe(true);
+    })
+})
