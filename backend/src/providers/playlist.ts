8518f988af6b0a90674191bd7275169bcc4efe31
David Robert-Ansart <david.robertansart@gmail.com>
2020-01-21 10:44:38 +0100

    :sparkles: Add spotify tracks route

---
+++ backend/src/providers/playlist.ts
@@ -0,0 +1,6 @@
+export class Playlist {
+  public description: string;
+  public id: string;
+  public name: string;
+  public image: string | null;
+}
--- backend/src/providers/spotify/playlists.ts
+++ backend/src/providers/spotify/playlists.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express';
 import request from 'request';
+import { Playlist } from '../playlist';
 
-interface SpotifyPlaylists {
+interface SpotifyPlaylistsResponse {
   href: string;
   items: Array<{
     description: string;
@@ -14,17 +15,17 @@ interface SpotifyPlaylists {
 }
 interface SpotifyPlaylistsBody {
   message: string;
-  playlists: SpotifyPlaylists;
+  playlists: SpotifyPlaylistsResponse;
 }
 
-interface NewPlaylists {
-  description: string;
-  id: string;
-  name: string;
-  image: string | null;
-}
-
-function requestPlaylists(token: string, userId: string | undefined): Promise<Array<NewPlaylists>> {
+/**
+ * Returns spotify playlists using user token access.
+ * If a userID is given, will fetch user playlists. Spotify featured playlists Otherwise.
+ *
+ * @param token : string, user access token that should be: 'bearer xxxxxxxx'.
+ * @param userId : string | undefined, user id.
+ */
+export function requestSpotifyPlaylists(token: string, userId: string | undefined): Promise<Array<Playlist>> {
   const getOptions = {
     url: userId !== undefined ? `https://api.spotify.com/v1/users/${userId}/playlists` : 'https://api.spotify.com/v1/browse/featured-playlists?country=FR',
     headers: {
@@ -33,15 +34,15 @@ function requestPlaylists(token: string, userId: string | undefined): Promise<Ar
     json: true
   };
 
-  return new Promise<Array<NewPlaylists>>((resolve, reject) => {
-    request.get(getOptions, (error, response, body: SpotifyPlaylistsBody | SpotifyPlaylists) => {
+  return new Promise<Array<Playlist>>((resolve, reject) => {
+    request.get(getOptions, (error, response, body: SpotifyPlaylistsBody | SpotifyPlaylistsResponse) => {
       if (!error && response.statusCode === 200) {
-        const playlists: Array<NewPlaylists> = [];
-        let spotifyPlaylists: SpotifyPlaylists;
+        const playlists: Array<Playlist> = [];
+        let spotifyPlaylists: SpotifyPlaylistsResponse;
         if (userId === undefined) {
           spotifyPlaylists = (body as SpotifyPlaylistsBody).playlists;
         } else {
-          spotifyPlaylists = body as SpotifyPlaylists;
+          spotifyPlaylists = body as SpotifyPlaylistsResponse;
         }
         for(const playlist of spotifyPlaylists.items) {
           playlists.push({
@@ -59,6 +60,12 @@ function requestPlaylists(token: string, userId: string | undefined): Promise<Ar
   });
 }
 
+/**
+ * Spotify playlists handler to get playlist at /spotify/playlists
+ *
+ * @param req
+ * @param res
+ */
 export async function getSpotifyPlaylists(req: Request, res: Response): Promise<void> {
   const token: string | undefined = req.header('Authorization');
   const userId: string | undefined = req.query.userId;
@@ -66,10 +73,10 @@ export async function getSpotifyPlaylists(req: Request, res: Response): Promise<
     throw ('Bearer authorization missing !')
   }
 
-  let playlists: Array<NewPlaylists>;
+  let playlists: Array<Playlist>;
 
   try{
-    playlists = await requestPlaylists(token, userId);
+    playlists = await requestSpotifyPlaylists(token, userId);
   } catch (e) {
     throw ('Error...')
   }
