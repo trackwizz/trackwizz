7f2eb45a35b65fa98f1703a2b64b8635bcdd8718
quentinverlhac <quentin.verlhac@gmail.com>
2020-01-26 15:39:03 +0100

    :label: Add QuestionUpdateMessage interface in front

--- frontend/src/websockets/MessageType.ts
+++ frontend/src/websockets/MessageType.ts
@@ -1,12 +1,24 @@
 enum MessageType {
   PING = "PING",
   JOIN_GAME = "JOIN_GAME",
+  QUESTION_UPDATE = "QUESTION_UPDATE",
   WAITING_ROOM_UPDATE = "WAITING_ROOM_UPDATE"
+}
+interface Answer {
+  id: string;
+  name: string;
+  artist: string;
 }
 
 export interface WaitingRoomUpdateMessage {
   type: MessageType.WAITING_ROOM_UPDATE;
   players: string[];
+}
+
+export interface QuestionUpdateMessage {
+  type: MessageType.QUESTION_UPDATE;
+  previewUrl: string;
+  answers: Answer[];
 }
 
 export default MessageType;
