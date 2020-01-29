86bd99795facc89c8f6c9da08fc0c221d6a28e8d
David Robert-Ansart <david.robertansart@gmail.com>
2020-01-29 20:36:29 +0100

    :pencil: Write spotify api doc

--- backend/src/utils/swagger.ts
+++ backend/src/utils/swagger.ts
@@ -1,7 +1,7 @@
 import swaggerJSDoc from "swagger-jsdoc";
 
 const apiOptions: swaggerJSDoc.Options = {
-  apis: ["./dist/controllers/*.js", "./src/controllers/*.yaml", "./dist/entities/*.js", "./src/entities/*.yaml"],
+  apis: ["./src/utils/*.yaml", "./src/providers/*.yaml", "./src/providers/*/**.yaml", "./src/controllers/*.yaml", "./src/entities/*.yaml"],
   swaggerDefinition: {
     info: {
       description: "This is the TrackWizz backend project:\nYou will find here all the docs for TrackWizz backend API.",
