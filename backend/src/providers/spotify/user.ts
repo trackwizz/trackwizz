abfa4ee527fe35f6388daf536f0d1da6db63a80c
quentinverlhac <quentin.verlhac@gmail.com>
2020-02-01 17:48:23 +0100

    :sparkles: create the user in the db after Spotify auth

---
+++ backend/src/providers/spotify/user.ts
@@ -0,0 +1,27 @@
+import request from "request";
+import { logger } from "../../utils/logger";
+
+interface SpotifyUser {
+  display_name: string;
+  id: string;
+}
+
+export function getSpotifyUser(accessToken: string): Promise<SpotifyUser> {
+  const authOptions = {
+    headers: {
+      Authorization: `Authorization: Bearer ${accessToken}`,
+    },
+    json: true,
+  };
+
+  return new Promise<SpotifyUser>((resolve, reject) => {
+    request.get("https://api.spotify.com/v1/me", authOptions, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        resolve(body);
+      } else {
+        logger.error(JSON.stringify(response));
+        reject();
+      }
+    });
+  });
+}
