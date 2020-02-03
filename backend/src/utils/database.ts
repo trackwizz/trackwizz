e8b4f2e60d54fb4274bd24b13e1ffc72ece9937d
Corentin Dupret <corentin.dupret@student.ecp.fr>
2020-02-03 16:56:07 +0100

    :card_file_box: Use URL directly for production DB

--- backend/src/utils/database.ts
+++ backend/src/utils/database.ts
@@ -1,32 +1,38 @@
 import path from "path";
-import { Connection, createConnection } from "typeorm";
+import { Connection, createConnection, ConnectionOptions } from "typeorm";
 import { logger } from "./logger";
 import { sleep } from "./index";
 
-const DBConfig = {
-  charset: "utf8mb4_unicode_ci",
-  database: process.env.DB_LIBRARY || "main",
-  entities: [path.join(__dirname, "../entities/*.js"), path.join(__dirname, "../entities/*.ts")],
-  extra:
-    process.env.NODE_ENV && process.env.NODE_ENV === "production"
-      ? {
-          ssl: true,
-        }
-      : {},
-  host: process.env.DB_HOST,
-  logging: false,
-  migrations: [path.join(__dirname, "../migration/**/*.js")],
-  password: process.env.DB_PASS,
-  port: parseInt(process.env.DB_PORT || "5432", 10),
-  subscribers: [path.join(__dirname, "../subscriber/**/*.js")],
-  synchronize: true,
-  timezone: "utc",
-  type: "postgres" as "postgres",
-  username: process.env.DB_USER,
-};
+let DBConfig: ConnectionOptions | null = null;
+// production
+if (process.env.DATABASE_URL) {
+  DBConfig = {
+    type: "postgres" as "postgres",
+    url: process.env.DATABASE_URL,
+    entities: [path.join(__dirname, "../entities/*.js"), path.join(__dirname, "../entities/*.ts")],
+    logging: false,
+    migrations: [path.join(__dirname, "../migration/**/*.js")],
+    subscribers: [path.join(__dirname, "../subscriber/**/*.js")],
+    synchronize: true,
+  };
+} else {
+  DBConfig = {
+    database: process.env.DB_LIBRARY || "main",
+    entities: [path.join(__dirname, "../entities/*.js"), path.join(__dirname, "../entities/*.ts")],
+    host: process.env.DB_HOST,
+    logging: false,
+    migrations: [path.join(__dirname, "../migration/**/*.js")],
+    password: process.env.DB_PASS,
+    port: parseInt(process.env.DB_PORT || "5432", 10),
+    subscribers: [path.join(__dirname, "../subscriber/**/*.js")],
+    synchronize: true,
+    type: "postgres" as "postgres",
+    username: process.env.DB_USER,
+  };
+}
 
 export async function connectToDatabase(tries: number = 10): Promise<Connection | null> {
-  if (tries === 0) {
+  if (tries === 0 || DBConfig === null) {
     return null;
   }
   let connection: Connection | null = null;
