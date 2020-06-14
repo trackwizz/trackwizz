e87f922be92a0be4aac91ed2cb025cca886a05cd
Alexandre <aflordlc@gmail.com>
2020-06-14 14:17:04 +0200

    Add TODO

--- backend/src/utils/transformTyping.ts
+++ backend/src/utils/transformTyping.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type requestQueryValue = any;
+// TODO : import QueryString so that any type can be removed and type can be as above
 // type requestQueryValue = string | QueryString.ParsedQs | string[] | QueryString.ParsedQs[] | undefined;
 
 export const transformTypingRequest = (requestQueryValue: requestQueryValue): string | undefined => {
